a736f4c97b3d5bf7541790a3fe8699a02ca02963
NikOdeemus <nicolejonmiller@gmail.com>
2021-10-26 01:21:55 -0500

    datacamp completion

---
+++ week_6_hw.sql
@@ -0,0 +1,32 @@
+1. Show all customers whose last names start with T. Order them by first name from A-Z.
+
+
+2. Show all rentals returned from 5/28/2005 to 6/1/2005
+
+
+3. How would you determine which movies are rented the most?
+
+
+4. Show how much each customer spent on movies (for all time) . Order them from least to most.
+
+
+5. Which actor was in the most movies in 2006 (based on this dataset)? Be sure to alias the actor name and count as a more descriptive name. 
+Order the results from most to least.
+
+
+6. Write an explaination for 4 and 5. Show the queries and explain what is happening in each one. Use the following link to understand how this works:
+http://postgresguide.com/performance/explain.html 
+
+
+7. What is the average rental rate per genre?
+
+
+8. How many films were returned late? Early? On time?
+
+9. What categories are the most rented and what are their total sales?
+
+10. Create a view for 8 and a view for 9. Be sure to name them appropriately. 
+
+
+Bonus:
+Write a query that shows how many films were rented each month. Group them by category and month. 
